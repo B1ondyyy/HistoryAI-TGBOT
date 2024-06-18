cbc020c1f03ab7d5065df1ff2f201588e5e8c123
B1ondyyy <stud0000267146@study.utmn.ru>
2024-06-18 23:36:12 +0500

    Add file

--- env.py
+++ env.py
@@ -1,4 +1,4 @@
 import os
 
-os.environ["BOT_TOKEN"] = "6720245580:AAGxrA24Gpa4X_b7f1cVFRyBVpz_lpHBjDY"      ##TODO Вставьте сюда токен своего бота Telegram
-os.environ["API_TOKEN"] = "ad948a4b-dd48-4612-8cc2-6963551f7b7c"        ##TODO Измените API токен модели на свой. Получить его можно по ссылке https://www.awanllm.com/keys
+os.environ["BOT_TOKEN"] = "6720245580:AAGxrA24Gpa4X_b7f1cVFRyBVpz_lpHBjDY"      # Вставьте сюда токен своего бота Telegram
+os.environ["API_TOKEN"] = "ad948a4b-dd48-4612-8cc2-6963551f7b7c"        # Измените API токен модели на свой. Получить его можно по ссылке https://www.awanllm.com/keys
